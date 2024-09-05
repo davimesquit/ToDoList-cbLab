107b29adc6a99d02bb588d8b059726ce048d5cef
Davi Mesquita <davimesquita.2004@gmail.com>
2024-09-04 23:19:58 -0300

    Create task template and improve home template

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, create
+from .views import home, create, tarefa
 
 urlpatterns = [
-    path('', index),
-    path('create/', create),
+    path('', home, name='home'),
+    path('create/', create, name='create'),
+    path('tarefa/<int:pk>/', tarefa, name='tarefa'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from .models import Tarefa
 
-def index(request):
+def home(request):
     tarefas = Tarefa.objects.all()
     
     context = {
         'title': 'Django To-Do',
         'tarefas': tarefas,
     }
-    return render(request, 'index.html', context)
+    return render(request, 'home.html', context)
 
 def create(request):
     return render(request, 'create.html')
+
+def tarefa(request, pk):
+    print(f'PK: {pk}')
+    tarefaId = Tarefa.objects.get(id=pk)
+    context = {
+        'tarefa': tarefaId,
+    }
+    return render(request, 'tarefa.html', context)
 
