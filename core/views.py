ebd03947bfd38f3762584767fdee9223aab5428c
Davi Mesquita <davimesquita.2004@gmail.com>
2024-09-07 01:04:22 -0300

    Implement pagination and search bar

--- core/views.py
+++ core/views.py
@@ -5,20 +5,47 @@ from .forms import TarefaModelForm
 from .forms import TarefaCreateModelForm
 from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
+from django.db.models import Q, Case, When, Value, IntegerField
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def home(request):
     tarefas = Tarefa.objects.all()
     
+    query = request.GET.get('query', '')
     ordenar_por = request.GET.get('ordenar', 'data')
     
-    if ordenar_por == 'data':
-        tarefas = tarefas.order_by('-dataCriacao')
+    if query:
+        tarefas = tarefas.filter(
+            Q(titulo__icontains=query) | Q(descricao__icontains=query)
+        )
+    
+    if ordenar_por == 'status':
+        tarefas = tarefas.order_by(
+            Case(
+                When(status='P', then=Value(0)),
+                default=Value(1),
+                output_field=IntegerField(),
+            ),
+            '-dataCriacao'
+        )
     else:
-        tarefas = tarefas.order_by('status')
+        tarefas = tarefas.order_by('-dataCriacao')
+    
+    paginator = Paginator(tarefas, 10)  # Mostra 10 tarefas por página
+    page = request.GET.get('page')
+
+    try:
+        tarefas_pag = paginator.page(page)
+    except PageNotAnInteger:
+        tarefas_pag = paginator.page(1)
+    except EmptyPage:
+        tarefas_pag = paginator.page(paginator.num_pages)
     
     context = {
+        'tarefasPg': tarefas_pag,
         'tarefas': tarefas,
         'ordenar_por': ordenar_por,
+        'query': query,
     }
     return render(request, 'home.html', context)
 
